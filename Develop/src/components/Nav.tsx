746396c97b7a10e57e70f7036967ec2ab699643a
ElBoyTM <nszalenski@gmail.com>
2024-11-15 16:37:34 -0500

    filled out nav page

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,8 +1,19 @@
-const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
-  return (
-    <div>Nav</div>
-  )
-};
+import { Link } from 'react-router-dom';
 
+  // TODO: Add necessary code to display the navigation bar and link between the pages
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Candidate Search</Link>
+        </li>
+        <li>
+          <Link to="/SavedCandidates">Saved Candidates</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+ 
 export default Nav;
