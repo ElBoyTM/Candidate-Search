ef63299eb54862b8b28ca52694e1e0032500728e
ElBoyTM <nszalenski@gmail.com>
2024-11-15 21:07:11 -0500

    made candidate interface

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,20 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+    readonly id: number;
+    readonly name: string;
+    readonly email: string;
+    readonly phone: string;
+    readonly city: string;
+    readonly state: string;
+    readonly zip: string;
+    readonly skills: string;
+    readonly photo: string;
+    readonly resume: string;
+    readonly linkedin: string;
+    readonly github: string;
+    readonly portfolio: string;
+    readonly status: string;
+    readonly notes: string;
+    readonly created_at: string;
+    readonly updated_at: string;
+}
